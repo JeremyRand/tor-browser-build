e726859459f8326d4e4e3075f124177626f09872
Jeremy Rand <jeremyrand@airmail.cc>
2020-12-11 12:01:15 +0000

    Clean cache after build

--- tools/cirrus_build_project.sh
+++ tools/cirrus_build_project.sh
@@ -30,10 +30,17 @@ else
     echo "Project cache miss, proceeding with build."
 fi
 
-# VM has 12 GB of free RAM.  Assuming each of the 4 logical cores takes 1 GB during build, that leaves us with 8 GB of unutilized RAM.  Alas, I'm not sure that's enough, so this isn't enabled right now.
+# VM has 12 GB of free RAM.  Assuming each of the 4 logical cores takes 1 GB
+# during build, that leaves us with 8 GB of unutilized RAM.  Alas, I'm not sure
+# that's enough, so this isn't enabled right now.
 #echo "Mounting tmpfs..."
 #mount -t tmpfs -o size=8G,nr_inodes=40k,mode=1777 tmpfs ./tmp
 #df -h
 
 echo "Building project..."
 ./rbm/rbm build "$PROJECT" --target "$CHANNEL" --target torbrowser-"$OS"-"$ARCH"
+
+# The cache has a size limit, so we need to clean useless data from it.  The
+# runc images are very large and seem to be fairly harmless to remove.
+echo "Cleaning cache..."
+rm -rfv out/container-image out/debootstrap-image
